b38440c6166a545980e3b66656c676b44f170410
12KaioPacheco6 <kaiopacheco094@gmail.com>
2025-05-06 15:51:21 -0300

    Create Cor.java

---
+++ SistemaVendas-Maven/prj-sistema-lavacao2025-javafx-parte1/src/main/java/br/edu/ifsc/fln/model/domain/Cor.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.edu.ifsc.fln.model.domain;
+
+public class Cor {
+    private int id;
+    private String cor;
+
+    public Categoria(String descricao) {
+        this.descricao = cor;
+    }
+
+    public Categoria() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return cor;
+    }
+
+    public void setDescricao(String cor) {
+        this.cor = cor;
+    }
+
+    @Override
+    public String toString() {
+        return cor;
+    }
+    
+}
